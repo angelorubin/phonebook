0fb0a527810358183fe879a337120a0312e9e713
angelorubin <angelorubin@gmail.com>
2018-06-10 19:40:40 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# phonebook CLI
+
+A CLI for phonebook.
+
+# License
+
+MIT - see LICENSE
+
---
+++ src/commands/phonebook.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'phonebook',
+  run: async context => {
+    const { print } = context
+
+    print.info('Welcome to your CLI')
+  }
+}
